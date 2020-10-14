01a215494cf57a860628befdbc1d53b71fd02341
Kakuwa <taras_grytsa@yahoo.com>
2020-10-14 10:07:51 +0100

    11

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -10,7 +10,20 @@ class TopMenu extends Component {
         return (
         <div className = 'app__header'>
             <img className = 'app_headerImage' src = {Logo} alt = 'XENA' />
-            <div className = 'app__login'>Login Here</div>
+            <div className = 'app__login'>
+                <input id = 'textEmail' type = "email" placeholder = "Email" />
+                <input id = 'textPassword' type = "password" placeholder = "Password" />
+                <button id = "btnLogin">
+                    Log In
+                </button>
+                <button id = "btnSignup">
+                    Sign Up
+                </button>
+                <button id = "btnLogout">
+                    Log Out
+                </button>
+            </div>
+            
           </div>
         );
     }
